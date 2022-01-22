989827c028209579f5788c2099d3b81820d6498a
Red-Machine-8 <70527887+Red-Machine-8@users.noreply.github.com>
2022-01-22 22:54:36 +0800

    Add project

---
+++ Credit/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Credit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выгрузитьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xLSXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьДанныеToolStripMenuItem,
+            this.выгрузитьДанныеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // загрузитьДанныеToolStripMenuItem
+            // 
+            this.загрузитьДанныеToolStripMenuItem.Name = "загрузитьДанныеToolStripMenuItem";
+            this.загрузитьДанныеToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.загрузитьДанныеToolStripMenuItem.Text = "Загрузить данные";
+            this.загрузитьДанныеToolStripMenuItem.Click += new System.EventHandler(this.загрузитьДанныеToolStripMenuItem_Click);
+            // 
+            // выгрузитьДанныеToolStripMenuItem
+            // 
+            this.выгрузитьДанныеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xMLToolStripMenuItem,
+            this.xLSXToolStripMenuItem});
+            this.выгрузитьДанныеToolStripMenuItem.Name = "выгрузитьДанныеToolStripMenuItem";
+            this.выгрузитьДанныеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выгрузитьДанныеToolStripMenuItem.Text = "Выгрузить данные";
+            // 
+            // xMLToolStripMenuItem
+            // 
+            this.xMLToolStripMenuItem.Name = "xMLToolStripMenuItem";
+            this.xMLToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.xMLToolStripMenuItem.Text = "XML";
+            this.xMLToolStripMenuItem.Click += new System.EventHandler(this.xMLToolStripMenuItem_Click);
+            // 
+            // xLSXToolStripMenuItem
+            // 
+            this.xLSXToolStripMenuItem.Name = "xLSXToolStripMenuItem";
+            this.xLSXToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.xLSXToolStripMenuItem.Text = "XLSX";
+            this.xLSXToolStripMenuItem.Click += new System.EventHandler(this.xLSXToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(630, 411);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(648, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(140, 251);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фильтр";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "1г";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "1в";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "1б";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "1а";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 171);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(128, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(648, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Удалить строку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Сведения о дебиторской и кредиторской задолжности";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выгрузитьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xLSXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Credit/Form1.cs
@@ -0,0 +1,330 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Credit
+{
+
+    public partial class Form1 : Form
+    {
+        String filePath = "";
+        DataTable dt = new DataTable();
+        NpgsqlDataAdapter adapter;
+        String updateRow = "Update reports SET x2 = '{0}', x3 = '{1}', x4 = '{2}', x5 = '{3}', x6 = '{4}', x7 = '{5}', x8 = '{6}', x9 = '{7}', x10 = '{8}', x11 = '{9}', x12 = '{10}', x13 = '{11}', x14 = '{12}' WHERE x1a = '{13}' AND x1b = '{14}' AND x1c = '{15}' AND x1d = '{16}'";
+        String insertRow = "INSERT INTO reports VALUES ('{0}', '{1}', '{2}', '{3}', {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16})";
+        NpgsqlConnection con = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=1234;Database=credits;");
+        public Form1()
+        {
+            InitializeComponent();
+            Connection();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Reload()
+        {
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            //dt = data.GetChanges();
+        }
+
+        private void Connection()
+        {
+
+            try
+            {
+                con.Open();
+                adapter = new NpgsqlDataAdapter("select * from reports", con);
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                Console.WriteLine("Соединение установлено!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка соединения");
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private Boolean Comparison(String[] data, int i)
+        {
+            if (dt.Rows.Count >= i)
+            {
+                if ((data[0] == dt.Rows[i][0].ToString()) && (data[1] == dt.Rows[i][1].ToString()) && (data[2] == dt.Rows[i][2].ToString()) && (data[3] == dt.Rows[i][3].ToString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    i += 1;
+                    return Comparison(data, i);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void загрузитьДанныеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] rowValue = { };
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = "c:\\";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                filePath = file.FileName;
+
+                var Stream = file.OpenFile();
+                StreamReader reader = new StreamReader(Stream, System.Text.Encoding.Default);
+                var row = reader.ReadLine();
+
+                while (row != null)
+                {
+                    if ((row[0] != '*') && (row[0] != '#') && (row != "ТБ=01"))
+                    {
+                        row = row.Replace(",", ".");
+                        rowValue = row.Split('|');
+                        rowValue = rowValue.Take(rowValue.Count() - 1).ToArray();
+                        if (Comparison(rowValue, 0))
+                        {
+                            string up = string.Format(updateRow, rowValue[4], rowValue[5], rowValue[6], rowValue[7], rowValue[8], rowValue[9], rowValue[10], rowValue[11], rowValue[12], rowValue[13], rowValue[14], rowValue[15], rowValue[16], rowValue[0], rowValue[1], rowValue[2], rowValue[3]);
+                            var cmd = new NpgsqlCommand(up, con);
+                            cmd.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            string ins = string.Format(insertRow, rowValue[0], rowValue[1], rowValue[2], rowValue[3], rowValue[4], rowValue[5], rowValue[6], rowValue[7], rowValue[8], rowValue[9], rowValue[10], rowValue[11], rowValue[12], rowValue[13], rowValue[14], rowValue[15], rowValue[16]);
+                            var cmd = new NpgsqlCommand(ins, con);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    row = reader.ReadLine();
+                }
+                reader.Close();
+                Reload();
+            }
+        }
+
+        private void xMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML files (*.xml)|*.xml";
+            if (sfd.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            string path = sfd.FileName;
+            ds.Tables.Add(dt);
+            ds.WriteXml(path, XmlWriteMode.IgnoreSchema);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlcmd;
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1a = '{0}' and x1b = '{1}' and x1c = '{2}' and x1d = '{3}'", textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text == "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1b = '{0}' and x1c = '{1}' and x1d = '{2}'", textBox2.Text, textBox3.Text, textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1c = '{0}' and x1d = '{1}'", textBox3.Text, textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text == "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1d = '{0}'", textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text != "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1c = '{0}'", textBox3.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text == "" && textBox2.Text != "" && textBox3.Text == "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1b = '{0}'", textBox2.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (textBox1.Text != "" && textBox2.Text == "" && textBox3.Text == "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1a = '{0}'", textBox1.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }else if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text == "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1a = '{0}' and x1b = '{1}'", textBox1.Text, textBox2.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }else if (textBox1.Text != "" && textBox2.Text == "" && textBox3.Text != "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1a = '{0}' and x1c = '{1}'", textBox1.Text, textBox3.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }else if (textBox1.Text != "" && textBox2.Text == "" && textBox3.Text == "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1a = '{0}' and x1d = '{1}'", textBox1.Text, textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }else if (textBox1.Text == "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text == "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1b = '{0}' and x1c = '{1}'", textBox2.Text, textBox3.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }else if (textBox1.Text == "" && textBox2.Text != "" && textBox3.Text == "" && textBox4.Text != "")
+            {
+                sqlcmd = string.Format("Select * From reports Where x1b = '{0}' and x1d = '{1}'", textBox2.Text, textBox4.Text);
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                sqlcmd = "Select * From reports for xml auto";
+                adapter = new NpgsqlDataAdapter(sqlcmd, con);
+                dt.Clear();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                    string cmd = String.Format("Delete From reports Where x1a = '{0}' and x1b = '{1}' and x1c = '{2}' and x1d = '{3}'", row.Cells[0].Value.ToString(), row.Cells[1].Value.ToString(), row.Cells[2].Value.ToString(), row.Cells[3].Value.ToString());
+                    var del = new NpgsqlCommand(cmd, con);
+                    dataGridView1.Rows.RemoveAt(row.Index);
+                    del.ExecuteNonQuery();
+            }
+            Reload();
+        }
+
+        private string klei(int i)
+        {
+            string str = dataGridView1[0, i].Value.ToString()+ " " + dataGridView1[1, i].Value.ToString() + " " + dataGridView1[2, i].Value.ToString() + " " + dataGridView1[3, i].Value.ToString();
+            return str;
+        }
+
+        private void Table(Excel.Worksheet wsh)
+        {
+            Excel.Range r1 = wsh.Range["A1", "A4"];
+            r1.Merge();
+            r1.Value = "Номер (код) счета бюджетного учета";
+            r1.EntireColumn.AutoFit();
+            r1.Borders.Color = ColorTranslator.ToOle(Color.Black);
+            r1 = wsh.Range["B1", "N1"];
+            r1.Merge();
+            r1.Value = "Сумма задолженности, руб";
+            r1.EntireColumn.AutoFit();
+            r1.HorizontalAlignment = HorizontalAlignment.Center;
+            r1 = wsh.Range["B2", "D2"];
+            r1.Merge();
+            r1.Value = "на начало года";
+            r1.EntireColumn.AutoFit();
+            r1.HorizontalAlignment = HorizontalAlignment.Center;
+            r1 = wsh.Range["B3", "B4"];
+            r1.Merge();
+            r1.Value = "всего";
+            r1.EntireColumn.AutoFit();
+            r1.HorizontalAlignment = HorizontalAlignment.Center;
+            r1 = wsh.Range["C3", "D3"];
+            r1.Merge();
+            r1.Value = "из них:";
+            r1.EntireColumn.AutoFit();
+            r1.HorizontalAlignment = HorizontalAlignment.Center;
+        }
+
+        private void xLSXToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Excel.Application exApp = new Excel.Application();
+
+            exApp.Workbooks.Open(Application.StartupPath.ToString() + "\\Шаблон.xlsx");
+            Excel.Worksheet wsh = exApp.Worksheets["Бюджет"];
+            //Table(wsh);
+            int i, j;
+            for (i = 0; i <= dataGridView1.RowCount - 2; i++)
+            {
+                for (j = 4; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    Console.WriteLine(klei(i));
+                    wsh.Cells[i + 6, 1] = klei(i);
+                    wsh.Cells[i + 6, 1].Borders.Color = ColorTranslator.ToOle(Color.Black);
+                    if(Convert.ToInt32(dataGridView1[j, i].Value) == 0)
+                    {
+                        wsh.Cells[i + 6, j - 2] = "-";
+                        wsh.Cells[i + 6, j - 2].Borders.Color = ColorTranslator.ToOle(Color.Black);
+                    }
+                    else
+                    {
+                        wsh.Cells[i + 6, j - 2] = dataGridView1[j, i].Value;
+                        wsh.Cells[i + 6, j - 2].Borders.Color = ColorTranslator.ToOle(Color.Black);
+                    }                    
+                }
+            }
+            exApp.Visible = true;
+        }
+    }
+}
